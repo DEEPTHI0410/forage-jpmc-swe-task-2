f12e9b1d2bcd3ee92f4cb1058ae367586f5a77e7
DEEPTHI0410 <89101967+DEEPTHI0410@users.noreply.github.com>
2023-05-20 19:04:35 +0530

    Task 2 commit

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -53,13 +53,13 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', '  
+      elem.setAttribute('aggregates', `
       {
           "stock": "distinct_count",
           "top_ask_price": "avg",
           "top_bid_price": "avg",
           "timestamp": "distinct count"
-        }');
+        }`);
 
     }
   }
